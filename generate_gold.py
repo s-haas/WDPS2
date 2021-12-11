2c13dbde9078a7420b726c4d360657e6254aa078
willemebe.vd.spek@gmail.com <willemebe.vd.spek@gmail.com>
2021-12-11 15:42:32 +0100

    Advances in BERT implementation

--- generate_gold.py
+++ generate_gold.py
@@ -1,48 +1,115 @@
-from google.cloud import language_v1
-from preprocessing import *
+import os
+import shutil
 
-def sample_analyze_sentiment(txt):
-    """
-    Analyzing Sentiment in a String
+import tensorflow as tf
+import pandas as pd
 
-    Args:
-      text_content The text content to analyze
-    """
+from transformers import BertTokenizer, TFBertForSequenceClassification
+from transformers import InputExample, InputFeatures
 
-    client = language_v1.LanguageServiceClient()
+URL = "https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
 
-    # text_content = 'I am so happy and joyful.'
 
-    # Available types: PLAIN_TEXT, HTML
-    type_ = language_v1.Document.Type.PLAIN_TEXT
+model = TFBertForSequenceClassification.from_pretrained("bert-base-uncased")
+tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 
-    # Optional. If not specified, the language is automatically detected.
-    # For list of supported languages:
-    # https://cloud.google.com/natural-language/docs/languages
-    language = "en"
-    document = {"content": txt, "type_": type_, "language": language}
+dataset = tf.keras.utils.get_file(fname="aclImdb_v1.tar.gz", 
+                                  origin=URL,
+                                  untar=True,
+                                  cache_dir='.',
+                                  cache_subdir='')
 
-    # Available values: NONE, UTF8, UTF16, UTF32
-    encoding_type = language_v1.EncodingType.UTF8
+def split_train_test(dataset):
+    main_dir = os.path.join(os.path.dirname(dataset), 'aclImdb')
+    # Create sub directory path ("/aclImdb/train")
+    train_dir = os.path.join(main_dir, 'train')
+    # Remove unsup folder since this is a supervised learning task
+    rm_dir = os.path.join(train_dir, 'unsup')
+    shutil.rmtree(rm_dir)
+    # View the final train folder
+    print(os.listdir(train_dir))
 
-    response = client.analyze_sentiment(request = {'document': document, 'encoding_type': encoding_type})
-    # Get overall sentiment of the input document
-    print(u"Document sentiment score: {}".format(response.document_sentiment.score))
-    print(
-        u"Document sentiment magnitude: {}".format(
-            response.document_sentiment.magnitude
+train = tf.keras.preprocessing.text_dataset_from_directory(
+    'aclImdb/train', batch_size=30000, validation_split=0.2, 
+    subset='training', seed=128)
+test = tf.keras.preprocessing.text_dataset_from_directory(
+    'aclImdb/train', batch_size=30000, validation_split=0.2, 
+    subset='validation', seed=128)
+
+
+def to_df(raw_data):
+    for i in raw_data.take(1):
+        feat = i[0].numpy()
+        lbl = i[1].numpy()
+
+    df = pd.DataFrame([feat, lbl]).T
+    df.columns = ['sentence', 'label']
+    df['sentence'] = df['sentence'].str.decode("utf-8")
+    return df
+
+def to_input(train, test, feat_col, lbl_col): 
+  train_input = train.apply(lambda x: InputExample(guid=None, # Globally unique ID for bookkeeping, unused in this case
+                                                          text_a = x[feat_col], 
+                                                          text_b = None,
+                                                          label = x[lbl_col]), axis = 1)
+
+  val_input = test.apply(lambda x: InputExample(guid=None, # Globally unique ID for bookkeeping, unused in this case
+                                                          text_a = x[feat_col], 
+                                                          text_b = None,
+                                                          label = x[lbl_col]), axis = 1)
+  
+  return train_input, val_input
+
+  train_InputExamples, validation_InputExamples = convert_data_to_examples(train, 
+                                                                           test, 
+                                                                           'text', 
+                                                                           'label')
+  
+def to_tf_dataset(examples, tokenizer, max_length=128):
+    features = [] # -> will hold InputFeatures to be converted later
+
+    for e in examples:
+        # Documentation is really strong for this method, so please take a look at it
+        input_dict = tokenizer.encode_plus(
+            e.text_a,
+            add_special_tokens=True,
+            max_length=max_length, # truncates if len(s) > max_length
+            return_token_type_ids=True,
+            return_attention_mask=True,
+            pad_to_max_length=True, # pads to the right by default # CHECK THIS for pad_to_max_length
+            truncation=True
         )
+
+        input_ids, token_type_ids, attention_mask = (input_dict["input_ids"],
+            input_dict["token_type_ids"], input_dict['attention_mask'])
+
+        features.append(
+            InputFeatures(
+                input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids, label=e.label
+            )
+        )
+
+    def gen():
+        for f in features:
+            yield (
+                {
+                    "input_ids": f.input_ids,
+                    "attention_mask": f.attention_mask,
+                    "token_type_ids": f.token_type_ids,
+                },
+                f.label,
+            )
+
+    return tf.data.Dataset.from_generator(
+        gen,
+        ({"input_ids": tf.int32, "attention_mask": tf.int32, "token_type_ids": tf.int32}, tf.int64),
+        (
+            {
+                "input_ids": tf.TensorShape([None]),
+                "attention_mask": tf.TensorShape([None]),
+                "token_type_ids": tf.TensorShape([None]),
+            },
+            tf.TensorShape([]),
+        ),
     )
-    # Get sentiment for all sentences in the document
-    for sentence in response.sentences:
-        print(u"Sentence text: {}".format(sentence.text.content))
-        print(u"Sentence sentiment score: {}".format(sentence.sentiment.score))
-        print(u"Sentence sentiment magnitude: {}".format(sentence.sentiment.magnitude))
 
-    # Get the language of the text, which will be the same as
-    # the language specified in the request or, if not specified,
-    # the automatically-detected language.
-    print(u"Language of the text: {}".format(response.language))
-
-df = read_sentences("/home/willem/Documents/CS/WDPS/WDPS2/data/datasetSentences.txt")
-sample_analyze_sentiment(df["sentence"].iloc[0])
