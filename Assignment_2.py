a34e29171b10f8f937fe60bc8faf17d819121e52
s-haas <52130274+s-haas@users.noreply.github.com>
2021-12-12 20:59:15 +0100

    Add files via upload

--- Assignment_2.py
+++ Assignment_2.py
@@ -8,61 +8,43 @@ Created on Sat Dec  4 10:02:10 2021
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
 from sklearn.ensemble import AdaBoostRegressor
 from sklearn.ensemble import GradientBoostingRegressor
 from Preprocessing import *
 
-"""
-def adjust_to_sentiment_range(model_output):
-    
-    return 2 * model_output - 1
 
-def adjust_to_model_range(model_input):
-    
-    return (model_input + 1) / 2
-"""
+def make_comparison_file(model, data, file_path, our_name):
+    data[our_name] = model.predict(data['sentence_encoded'])
+    data.to_csv(file_path, sep='|')
+    return data
 
-def train_model(model_class, train_x, train_y):
-    #adjust_to_model_range(train_y)
-    model_class.fit(train_x, train_y)
-    return model_class
-
-def results_model(comparison_table, col_names):
-    conf_mat = confusion_matrix(comparison_table[col_names['our']], comparison_table[col_names['gold']])
-    return conf_mat
-
-def make_gold_file(gold_model, model_input, entities, gold_name):
-    gold_table = pd.DataFrame()
-    gold_table['Entity'] = entities
-    gold_table[gold_name] = gold_model.predict(model_input)
-    return gold_table
-
-def make_comparison_file(model, model_input, gold_table, file_path, our_name):
-    comparison_table = gold_table.copy()
-    comparison_table[our_name] = model.predict(model_input)
-    comparison_table.to_csv(file_path, sep='\t')
-    return comparison_table
-
-
-def main():
+def preprocessing_and_embedding():
     sents = read_sentences('data/datasetSentences.txt')
-    #print(sents.head())
     embedding_model = train_word_2_vec(list(sents['sentence']))
     preprocess(sents, 'sentence', 'data/preprocessed_sentences.csv')
     encode(sents, 'sentence', embedding_model)
+    return sents
+
+def read_encoding_and_sentiments():
+    input_sents = pd.read_csv('data/encoded_sentences.csv', sep='|')
+    sentiments = pd.read_csv('data/BERT_output.csv', sep='|')
+    input_sents = input_sents.merge(sentiments)
+    input_sents.to_csv('data/model_input.csv', sep='|')
+    return input_sents
+
+def main():
+    #input_sents = preprocessing_and_embedding()
+    #input_sents = pd.read_csv('data/model_input.csv', sep='|')
+    input_sents = read_encoding_and_sentiments()
+    
     model1 = GradientBoostingRegressor(loss='squared_error', learning_rate=0.1, n_estimators=100, subsample=1.0, criterion='friedman_mse', min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_depth=3, min_impurity_decrease=0.0, init=None, random_state=None, max_features=None, alpha=0.9, verbose=0, max_leaf_nodes=None, warm_start=False, validation_fraction=0.1, n_iter_no_change=None, tol=0.0001, ccp_alpha=0.0)
     model2 = AdaBoostRegressor(base_estimator=None, n_estimators=50, learning_rate=1.0, loss='linear', random_state=None)
     
-    print(sents.head())
-    # create train-validate-test split
-    #make_gold_file()
-    # our model
-    """
-    train_model()
-    make_comparison_file()
-    results_model()
-    """
+    train_data, test_data = train_test_split(input_sents, train_size=0.7, random_state = 42)
+    model1.fit(train_data['sentence_encoded'], train_data['score'])
+    make_comparison_file(model1, test_data, 'data/comp.csv', 'predicted_sentiment')
+    
+    
     return
 
 
--- Preprocessing.py
+++ Preprocessing.py
@@ -1,4 +1,3 @@
-import nltk
 import string
 
 import pandas as pd
@@ -6,7 +5,6 @@ import numpy as np
 
 from gensim.models import Word2Vec
 from nltk.tokenize import word_tokenize
-from nltk.corpus import brown
 
 """
 def read_csv(fpath, delimiter='|'):
@@ -22,7 +20,7 @@ def preprocess(raw_df, column_name, fpath):
     raw_df[column_name] = raw_df[column_name].apply(remove_punct)
     raw_df[column_name] = raw_df[column_name].apply(lambda x: x.lower())
     
-    raw_df.to_csv(fpath, sep='|')
+    #raw_df.to_csv(fpath, sep='|')
 
 def encode_sentence(sentence, model):
     sent_mat = [model.wv[token] for token in word_tokenize(sentence)]
@@ -33,35 +31,14 @@ def encode(df, column_name, model, fpath='data/encoded_sentences.csv'):
     df[column_name + '_encoded'] = df[column_name].apply(encode_sentence, model=model)
     df.to_csv(fpath, sep='|')
     return
-"""
-def extract_entity_sentence_pairs(): 
-    return
-"""
+
 def remove_punct(txt):
     return "".join([w for w in txt if w not in string.punctuation])
 
 def train_word_2_vec(input_sentences):
-    # copied
-    #print(input_sentences)
-    nltk.download("brown")
-    nltk.download('punkt')
-    data = []
-    #doc = [word_tokenize(sent) for sent in list(brown.sents())]
-    doc = list(brown.sents())
     lower = [sent.lower() for sent in input_sentences]
     without_punct = [remove_punct(sent) for sent in lower]
     dataset = [word_tokenize(sent) for sent in without_punct]
-    doc.extend(dataset)
-    """
-    for sent in doc:
-        new_sent = []
-        for word in sent:
-            new_word = word.lower()
-            if new_word[0] not in string.punctuation:
-                new_sent.append(new_word)
-        if len(new_sent) > 0:
-            data.append(new_sent)
-    """
     data = dataset
     
     model = Word2Vec(
