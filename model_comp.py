83383643651df606f0eb1692755bcdb0736b7dd8
s-haas <52130274+s-haas@users.noreply.github.com>
2021-12-21 18:03:04 +0100

    Add files via upload

---
+++ model_comp.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  4 10:02:10 2021
+
+@author: stefan
+"""
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import AdaBoostRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.linear_model import LogisticRegression
+from preprocessing import *
+
+
+def make_comparison_file(model, data, file_path, our_name):
+    """
+
+    Parameters
+    ----------
+    model : sentiment prediction/detection model
+    data : dataframe with encoded/embedded test sentences
+    file_path : path to file the dataframe with predictions will be written to
+    our_name : name the column with predicted sentiments is going to have
+
+    Returns
+    -------
+    data : dataframe with a column added that contains predicted sentiments
+
+    """
+    
+    data[our_name] = model.predict(np.array(data['sentence_encoded']).reshape(-1,1))
+    data.to_csv(file_path, sep='|')
+    return data
+
+def preprocessing_and_embedding():
+    """
+    reads the file with sentences, preprocesses them and encodes/embeds them
+    does the same for the sentences scraped from rottentomatoes, but does
+    not concatenate the resulting dataframes, instead only returning the one 
+    with the training data and writing both to different files
+    Returns
+    -------
+    sents : a dataframe containing the sentence_ids, preprocessed sentences,
+            sentence encodings/embeddings
+
+    """
+    sents = read_sentences('data/datasetSentences.txt')
+    #final_sents = pd.read_json('data/rottentomatoes.json')
+    embed_sents = list(sents['sentence'])#.extend(list(final_sents['content']))
+    embedding_model = train_word_2_vec(embed_sents)
+    preprocess(sents, 'sentence', 'data/preprocessed_sentences.csv')
+    #preprocess(final_sents, 'content', 'data/preprocessed_final_sentences.csv')
+    encode(sents, 'sentence', embedding_model)
+    #encode(final_sents, 'content', embedding_model, 'data/encoded_final_sentences.csv')
+    return sents
+
+def read_encoding_and_sentiments(input_sents=None):
+    """
+
+    Parameters
+    ----------
+    input_sents : dataframe containing the input sentences and their encodings/embeddings
+                  The default is None.
+                  if set to None, reads from default file
+
+    Returns
+    -------
+    input_sents : dataframe containing the input sentences, their encoding/embedding and
+                  their predicted sentiments according to the BERT model
+
+    """
+    if input_sents is None:
+        input_sents = pd.read_csv('data/encoded_sentences.csv', sep='|')
+    sentiments = read_sentences('data/predictions.tsv')
+    sentiments.drop('sentence', axis=1, inplace=True)
+    input_sents = pd.merge(input_sents, sentiments, how='left', on='sentence_index')
+    input_sents.to_csv('data/model_input.csv', sep='|')
+    return input_sents
+
+def mae(model_col, label_col):
+    """
+
+    Parameters
+    ----------
+    model_col : column of the dataframe containing the sentiments predicted by our model
+    label_col : column of the dataframe containing the sentiments predicted by BERT
+
+    Returns
+    -------
+    The mean absolute error of the models' predictions compared to those of BERT
+
+    """
+    diff = abs(model_col - label_col)
+    return np.average(diff.values)
+
+def main():
+    """
+
+    Returns
+    -------
+    the predictions our model makes for new data
+
+    """
+    #prevent SettingWithCopyWarning message from appearing
+    pd.options.mode.chained_assignment = None
+    
+    input_sents = preprocessing_and_embedding()
+    #input_sents = pd.read_csv('data/model_input.csv', sep='|')
+    input_sents = read_encoding_and_sentiments(input_sents)
+    #model1 = GradientBoostingRegressor(n_estimators=100)
+    #model2 = AdaBoostRegressor(n_estimators=50)
+    model3 = LogisticRegression()
+    train_data, test_data = train_test_split(input_sents, train_size=0.7, random_state = 42)
+    
+    model = model3
+    model.fit(np.array(train_data['sentence_encoded']).reshape(-1,1), train_data['label'])
+    comp = make_comparison_file(model, test_data, 'data/comp.csv', 'predicted_sentiment')
+    
+    #print(mae(comp['predicted_sentiment'], comp['label']))
+    """
+    final_sents = pd.read_csv('data/encoded_final_sentences.csv')
+    final_sents['pred'] = model.predict(np.array(final_sents['content_encoded']).reshape(-1,1))
+    """
+    return final_sents
+
+
+if __name__ == '__main__':
+    main()
---
+++ preprocessing.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  4 10:02:10 2021
+
+@author: stefan
+"""
+
+import string
+
+import pandas as pd
+import numpy as np
+
+from gensim.models import Word2Vec
+from nltk.tokenize import word_tokenize
+
+"""
+def read_csv(fpath, delimiter='|'):
+    raw_data = pd.read_csv(fpath, delimiter=delimiter, header=None, index_col=1)
+    raw_data.rename(columns={0:'Phrase'}, inplace=True)
+    return raw_data
+"""
+def read_sentences(sentence_file):
+    """
+
+    Parameters
+    ----------
+    sentence_file : path to tsv file containing sentences and sentence ids
+
+    Returns
+    -------
+    sentences : pandas dataframe made from the sentence file
+
+    """
+    sentences = pd.read_csv(sentence_file, sep='\t', index_col='sentence_index')
+    return sentences
+
+def preprocess(raw_df, column_name, fpath):
+    """
+
+    Parameters
+    ----------
+    raw_df : dataframe containing 'raw' sentences
+    column_name : name of the column in the dataframe containing 'raw' sentences
+    fpath : path to file where the adjusted dataframe is written to
+
+    Returns
+    -------
+    preprocesses the sentences in the given column name and replaces them with
+    the preprocessed sentences
+
+    """
+    raw_df[column_name] = raw_df[column_name].apply(remove_punct)
+    raw_df[column_name] = raw_df[column_name].apply(lambda x: x.lower())
+    
+    raw_df.to_csv(fpath, sep='|')
+    return
+
+def encode_sentence(sentence, model):
+    """
+
+    Parameters
+    ----------
+    sentence : sentence to be encoded/embedded
+    model : model used for encoding/embedding
+            in this case word2vec
+
+    Returns
+    -------
+    sentence encoded/embedded into a number
+
+    """
+    sent_mat = [model.wv[token] for token in word_tokenize(sentence)]
+    #sent_mat = np.hstack(sent_mat)
+    return sum(np.add.reduce(sent_mat))
+
+def encode(df, column_name, model, fpath='data/encoded_sentences.csv'):
+    """
+
+    Parameters
+    ----------
+    df : dataframe containing the sentence information
+    column_name : name of the column containing the preprocessed sentences
+    model : model used for encoding/embedding
+            in this case word2vec
+    fpath : path to file where the adjusted dataframe is written to
+            The default is 'data/encoded_sentences.csv'.
+
+    Returns
+    -------
+    encodes/embeds all sentences to be used for the model
+
+    """
+    df[column_name + '_encoded'] = df[column_name].apply(encode_sentence, model=model)
+    df.to_csv(fpath, sep='|')
+    return
+
+def remove_punct(txt):
+    """
+
+    Parameters
+    ----------
+    txt : sentence
+
+    Returns
+    -------
+    sentence with punctuation characters removed
+
+    """
+    return "".join([w for w in txt if w not in string.punctuation])
+
+def train_word_2_vec(input_sentences):
+    """
+
+    Parameters
+    ----------
+    input_sentences : array of sentences to be encoded/embedded
+
+    Returns
+    -------
+    model : word2vec model trained with all sentences
+
+    """
+    lower = [sent.lower() for sent in input_sentences]
+    without_punct = [remove_punct(sent) for sent in lower]
+    dataset = [word_tokenize(sent) for sent in without_punct]
+    data = dataset
+    
+    model = Word2Vec(
+        sentences = data,
+        vector_size = 50,
+        window = 10,
+        epochs = 20,
+        min_count = 0
+    )
+
+    return model
