d19643216bec5c823e90a292f1b530b01c95205d
willemebe.vd.spek@gmail.com <willemebe.vd.spek@gmail.com>
2021-12-22 17:25:01 +0100

    Progress in main wrapper

--- ui.py
+++ ui.py
@@ -1,5 +1,28 @@
+import numpy as np
+import json
+import tempfile
+
 from rottentomatoes import search_for_movies, reviews_for_movie, reviews_of_critic
 from ui_chooser import choose
+
+def json_to_file(json_obj, fpath="./data/critic_reviews.json"):
+    if not fpath.endswith(".json"):
+        return False
+    with open(fpath, 'w', encoding='utf-8') as f:
+        json.dump(json_obj, f, ensure_ascii=False, indent=2)
+    return True
+
+
+def gather_result(pred):
+    '''
+    Gather the sentiment according to the model's predictions. 
+    '''
+    sents = ["Very Negative", "Somewhat Negative", "Neutral", "Somewhat Positive", "Very Positive"]
+    mean_sentiment = np.mean(pred)
+    sent_text = sents[(np.abs(np.linspace(0, 1, len(sents)) 
+                              - mean_sentiment)).argmin()] # Gather sentiment string closest to mean.
+    return mean_sentiment, sent_text
+
 
 def main():
     movies = search_for_movie()
@@ -7,7 +30,10 @@ def main():
     review = choose_review(chosen_movie, reviews)
 
     # print(f"\n{review['critic_name']} is typically very negative (-0.968)") # MOCK
-    print(reviews_of_critic(review["critic_id"]))
+    json_to_file(reviews_of_critic(review["critic_id"]))
+    # pred = model()
+    # sentiment, sentiment_text = gather_result(pred)
+    # print(f"\n{review['critic_name']} is typically {sentiment_text} ({sentiment})")
 
 def search_for_movie():
     while(True):
@@ -18,6 +44,7 @@ def search_for_movie():
             print(f"No movies found for query “{movie_query}”. Please try a different query.")
         else:
             return movies
+
 
 def choose_movie(movie_suggestions):
     while(True):
@@ -31,8 +58,7 @@ def choose_movie(movie_suggestions):
     
 
 def choose_review(movie, reviews):
-    return choose(reviews, lambda rv: f"{rv['critic_name']}: {rv['content']}", prompt=f"\nReviews for {movie['title']}")
-
+    return choose(reviews, lambda rv: f"{rv['critic_name']}: {rv['content']}", prompt=f"\nReviews for {movie['title']}")    
 
 
 if __name__ == "__main__":
