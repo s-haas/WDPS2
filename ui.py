7de2b71cb25120017d7af95425d427ee04ead3c7
leonhard <l.wattenbach@gmail.com>
2021-12-22 15:35:22 +0100

    Handle special case that there are no movies/reviews to choose from

--- ui.py
+++ ui.py
@@ -1,25 +1,36 @@
 from rottentomatoes import search_for_movies, reviews_for_movie, reviews_of_critic
 from ui_chooser import choose
 
-
-
 def main():
     movies = search_for_movie()
-    chosen_movie = choose_movie(movies)
-    review = choose_review(chosen_movie)
+    chosen_movie, reviews = choose_movie(movies)
+    review = choose_review(chosen_movie, reviews)
 
     # print(f"\n{review['critic_name']} is typically very negative (-0.968)") # MOCK
     print(reviews_of_critic(review["critic_id"]))
 
 def search_for_movie():
-    movie_query = input("Search for a movie:\n")
-    return search_for_movies(movie_query)
+    while(True):
+        movie_query = input("Search for a movie:\n")
+        movies = search_for_movies(movie_query)
+
+        if len(movies) == 0:
+            print(f"No movies found for query “{movie_query}”. Please try a different query.")
+        else:
+            return movies
 
 def choose_movie(movie_suggestions):
-    return choose(movie_suggestions, lambda movie: f"{movie['title']} ({movie['year']}; {', '.join(movie['cast'])})")
+    while(True):
+        movie = choose(movie_suggestions, lambda movie: f"{movie['title']} ({movie['year']}; {', '.join(movie['cast'])})")
+        reviews = reviews_for_movie(movie['id'])
 
-def choose_review(movie):
-    reviews = reviews_for_movie(movie['id'])
+        if len(reviews) == 0:
+            print(f"{movie['title']} has no reviews. Please choose a different movie.")
+        else:
+            return (movie, reviews)
+    
+
+def choose_review(movie, reviews):
     return choose(reviews, lambda rv: f"{rv['critic_name']}: {rv['content']}", prompt=f"\nReviews for {movie['title']}")
 
 
--- ui_chooser.py
+++ ui_chooser.py
@@ -1,6 +1,9 @@
 import enquiries
 
 def choose(options, label_formatter = lambda x: x, prompt = "Choose one of these options: "):
+    if len(options) == 0:
+        return None
+        
     option_labels = [f"[{index + 1}] {label_formatter(obj)}"[:100] for (index, obj) in enumerate(options)]
     chosen_label = enquiries.choose(prompt, option_labels)
     return options[option_labels.index(chosen_label)]
